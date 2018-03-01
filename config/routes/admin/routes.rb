00c99e7a4da0aa5ca93519c11d354f24a266cf15
齋藤美恵 <yuunyan04@gmail.com>
2018-03-01 19:26:25 +0900

    fix routing

--- config/routes/admin/routes.rb
+++ config/routes/admin/routes.rb
@@ -1,16 +1,7 @@
 Rails.application.routes.draw  do
   namespace :admin do
-    get '', to: 'signup#index'
-
-    resources :signup, only: :index do
-      get :do, on: :collection, as: 'do'
-    end
-
-    resources :invitation do
-      member do
-      end
-      collection do
-      end
+    get '', to: 'invitation#index'
+    resources :invitation, only: %w(index show) do
     end
   end
 end
--- config/routes/user/routes.rb
+++ config/routes/user/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'user/wedding#index'
   scope module: :user do
     resources :wedding, only: :index
   end
